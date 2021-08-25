9ea386bd2b4969fcae97fa4271a8e177041aeedb
RobertoCarlosMedina <robertocarlosmedina.dev@gmail.com>
2021-08-25 11:03:02 -0100

    update

--- gitToken.py
+++ gitToken.py
@@ -18,14 +18,12 @@ class Push:
         os.system("git commit -m 'update'")
         
         os.system("git push")
-        # os.system("echo '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'")
 
 class Auth:
     def __init__(self):
         pass
 
     def run(self):
-        # os.system("echo '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'")
         token = "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"
         array = list(token)
         sleep(2)
@@ -43,21 +41,12 @@ class Auth:
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
 
-        # sleep(2)
-
-        # keyboard.press('_')
-        # keyboard.release('_')
-        # sleep(2)
-        # keyboard.press('0')
-        # keyboard.release('0')
-
 if (password == authPass):
     array= [Push(), Auth()]
 
     for item in array:
         th = ThreadTask(item)
         th.start()
-    # print("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")
 
 else:
     print("auth: validation error")
