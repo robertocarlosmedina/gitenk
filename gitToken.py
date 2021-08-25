9474290c82bb6a4952a060a5912479184aa88bc8
RobertoCarlosMedina <robertocarlosmedina.dev@gmail.com>
2021-08-25 10:13:41 -0100

    Starting repo

---
+++ gitToken.py
@@ -0,0 +1,15 @@
+import os
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+# password = input("Auth: ")
+password = ""
+authPass = ""
+
+if (password == authPass):
+    os.system("git push")
+    print("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")
+
+else:
+    print("auth: validation error")
