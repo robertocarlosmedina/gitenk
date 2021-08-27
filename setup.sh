0622524618a296e79a77112271fa28ac72a129bf
RobertoCarlosMedina <robertocarlosmedina.dev@gmail.com>
2021-08-27 14:35:07 -0100

    Push to repo test

--- setup.sh
+++ setup.sh
@@ -1,14 +1,26 @@
+
 # gitenk dependencies
 # Setup and dependencies installation
+credentials = ""
+if [ -z "$1"]
+then
+    echo -n "PASTE YOUR CREDENTIALS TOKEN HERE: "
+    read credentials
+else
+    credentials = $1
+fi
+
 apt install figlet toilet
 pip3 install pynput
 sudo apt install lolcat
 sudo cp ./gitenk.py /bin/
 sudo cp ./gitenk /bin/
-echo "PASTE_YOUR_CREDENTIALS_TOKEN_HERE" > credentials.t    
+echo "$credentials" > credentials.txt   
+
+clear
+
 figlet -f slant -k -c  gitenk | lolcat -a -d 1
 figlet -f term -k -c  Roberto Carlos Medina | lolcat -a -d 1
 figlet -f term -k -c  robertocarlosmedina.dev@gmail.com | lolcat
 figlet -f circle -k -c  2021 | lolcat
-echo "Use 'gitenk help' to get help."xt
-
+figlet -fterm -c Use 'gitenk help' to get start. | lolcat -a
