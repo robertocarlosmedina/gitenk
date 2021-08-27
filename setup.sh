e2565becbd0a3a80bf9a4b32a19b8fdcd3ec1ae8
RobertoCarlosMedina <robertocarlosmedina.dev@gmail.com>
2021-08-27 14:23:27 -0100

    Updating

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# gitenk dependencies
+# Setup and dependencies installation
+apt install figlet toilet
+pip3 install pynput
+sudo apt install lolcat
+sudo cp ./gitenk.py /bin/
+sudo cp ./gitenk /bin/
+echo "PASTE_YOUR_CREDENTIALS_TOKEN_HERE" > credentials.t    
+figlet -f slant -k -c  gitenk | lolcat -a -d 1
+figlet -f term -k -c  Roberto Carlos Medina | lolcat -a -d 1
+figlet -f term -k -c  robertocarlosmedina.dev@gmail.com | lolcat
+figlet -f circle -k -c  2021 | lolcat
+echo "Use 'gitenk help' to get help."xt
+
