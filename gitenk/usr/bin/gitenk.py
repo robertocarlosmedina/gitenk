03fe0351d6f6316532d6faed1ef09fedaee4712d
RobertoCarlosMedina <robertocarlosmedina.dev@gmail.com>
2021-08-30 11:21:45 -0100

    Pakage commit test
    Testin the pakage whit a real commit to this repo.

--- gitenk/usr/bin/gitenk.py
+++ gitenk/usr/bin/gitenk.py
@@ -5,12 +5,13 @@ from pynput.keyboard import Key, Controller
 from threading import Thread
 
 
-class ThreadOFTask(Thread):
+class FunctionThreadTask(Thread):
     def __init__(self, function_sent):
         Thread.__init__(self)
         self.function_toExec = function_sent
     
     def run(self):
+        self.function_toExec()
         self.function_toExec()
 
 class ThreadOSTask(Thread):
@@ -33,13 +34,11 @@ class Auth:
     def readCredentials(self):
         file_credentials = open("/usr/bin/credentials.txt", "r")
         line = file_credentials.readlines()
-        file_credentials.close()
+        # file_credentials.close()
         return line[0]
 
     def getUseCredentials(self):
-        token = list(self.readCredentials())
-        # print(self.readCredentials())
-
+        token = self.readCredentials()
         sleep(3)
         for chr in token:
             self.keyboard.press(chr)
@@ -59,22 +58,44 @@ class Auth:
         file_credentials.write(newToken)
         file_credentials.close()
 
+
 class GitAction:
     def __init__(self):
         self.auth = Auth()
+        self.path = None
+        self.commitHeader = None
+        self.commitMessage = None
+    
+    def setUPCommitValues(self):
+        self.path = input("File path: ")
+        if (self.path == ""):
+            self.path == "."
+
+        self.commitHeader = input("Commit header: ")
+        if (self.commitHeader == ""):
+            self.commitHeader = None
+
+        self.commitMessage = input("Commit description: ")
+        if (self.commitMessage == ""):
+            self.commitMessage = None
+
+        if(self.commitHeader == None and self.commitMessage == None):
+            return False
+
+        return True
     
     def authentication(self):
         self.auth.getUseCredentials()
 
     def push(self):        
-        path = input("File path: ")
-        commitHeader = input("Commit header: ")
-        commitMessage = input("Commit description: ")
-        os.system(f"git add {path}")
-        if (commitHeader != None):
-            os.system(f"git commit -m '{commitHeader}' -m '{commitMessage}'")
+        os.system(f"git add {self.path}")
+        if (self.commitHeader != None and self.commitMessage != None):
+            os.system(f"git commit -m '{self.commitHeader}' -m '{self.commitMessage}'")
+        elif(self.commitHeader == None):
+            os.system(f"git commit -m '{self.commitMessage}'")
         else:
-            os.system(f"git commit -m '{commitMessage}'")
+            os.system(f"git commit -m '{self.commitHeader}'")
+
         os.system("git push")
 
     def pull(self):
@@ -87,19 +108,26 @@ class GitAction:
         newToken = input("New Token: ")
         self.auth.changeCedentials(newToken)
 
+        # os.system("clear")
 
 gitAction = GitAction()
-cmds_dict = {"push":[gitAction.push], "pull":[gitAction.pull, gitAction.authentication], "show":[gitAction.showCredentials],\
+cmds_dict = {"push":[gitAction.push, gitAction.authentication], "pull":[gitAction.pull, gitAction.authentication], "show":[gitAction.showCredentials],\
      "change":[gitAction.changeCredentials]}
 
 if (len(sys.argv)>1):
+    validOperation = True
     for cmd, actions in cmds_dict.items():
         if(sys.argv[1]==cmd):
-            if(len(actions)>1):
-                for act in actions:
-                    th_task = ThreadOFTask(act)
-                    th_task.start()
+
+            if (cmd == "push"):
+                validOperation = gitAction.setUPCommitValues()
+
+            if validOperation:
+                if(len(actions)>1):
+                    for act in actions:
+                        th_task = FunctionThreadTask(act)
+                        th_task.start()
+                else:
+                    actions[0]()
             else:
-                actions[0]()
-else:
-    print("Invalid command")
+                print("\nAction Error: try it again.")
